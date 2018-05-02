3071353b2949f0dfcc75ec87ec331db5e86ba08d
klept0 <klept0iz@gmail.com>
2018-05-02 08:55:07 -0400

    Create mint.com.user.js

---
+++ mint.com.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name        Cleanup/Hide Categories on Mint
+// @namespace   Cleanup Mint
+// @description Cleans up categories on Mint.com
+// @author      klept0
+// @include     https://wwws.mint.com/*
+// @include     https://mint.intuit.com/*
+// @version     1.2
+// @grant       none
+// ==/UserScript==
+console.log('Greasemonkey Start');
+var Greasemonkey = function () {
+  console.log('running Greasemonkey Script');
+  var toBlock = [
+    'div.AdviceView',
+    'div.AlertsView',
+    'div.CreditScoreView',
+    'div.BillRemindersView',
+    'div.w2sView',
+    'div.TaxesView',
+    'div.GoalsView',
+    'div.InvestmentsView'
+  ];
+  function removeElems() {
+    if (typeof jQuery === 'undefined') {
+      return;
+    }
+    jqObjs = jQuery(toBlock.join(','));
+    jqObjs.each(function(i, x) {
+      var idx = toBlock.indexOf('div.'+jQuery(this).attr('class'));
+      if (idx == -1) { return; }
+      toBlock.splice(idx, 1);
+    });
+    jqObjs.remove();
+    if(toBlock.length == 0) {
+      console.log('Done.');
+      clearInterval(interval);
+    }
+  }
+  var interval = setInterval(removeElems, 100);
+}();
